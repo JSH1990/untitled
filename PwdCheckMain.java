d46ad0fb3071bd4afa9c193370e0c62c803691c7
JSH1990 <118979741+JSH1990@users.noreply.github.com>
2023-01-18 15:03:52 +0900

    Add files via upload

--- PwdCheckMain.java
+++ PwdCheckMain.java
@@ -12,12 +12,29 @@ public class PwdCheckMain {
             System.out.print("암호 입력 : ");
             pwd = sc.next();
 
-            if(pwd.equals("종료") || pwd.equalsIgnoreCase("exit")) break;
+            if (pwd.equals("종료") || pwd.equalsIgnoreCase("exit")) break;
 
-            if (pwdCheck.validLength(pwd) == false) {
+            if (!pwdCheck.validLength(pwd)) {
                 System.out.println("bad length password");
                 continue;
             }
+            if (!pwdCheck.validNumber(pwd)) {
+                System.out.println("bad number password");
+                continue;
+            }
+            if (!pwdCheck.validLowerAlphabet(pwd)) {
+                System.out.println("bad Lower password");
+                continue;
+            }
+            if (!pwdCheck.validUpperAlphabet(pwd)) {
+                System.out.println("bad Upper password");
+                continue;
+            }
+            if (!pwdCheck.validRex(pwd)) {
+                System.out.println("bad Rex password");
+            continue;
+            }
+            System.out.println("good password");
         }
     }
-}
+    }
