a8214c4c1e663cfe5b164498fc5197f15a6de4fc
JSH1990 <118979741+JSH1990@users.noreply.github.com>
2023-01-25 15:24:16 +0900

    Add files via upload

---
+++ MultiInterMain.java
@@ -0,0 +1,10 @@
+package 다중인터페이스;
+
+public class MultiInterMain {
+    public static void main(String[] args) {
+        SportCar sportCar = new SportCar(true, 22, true, 220,
+                "red", "2022", true);
+        sportCar.viewInfo();
+    }
+    }
+
--- SportCar.java
+++ SportCar.java
@@ -1,16 +1,58 @@
-package 자동차종합예제;
+package 다중인터페이스;
 
-public class SportCar extends Car {
-    public SportCar(String name) {
-        this.name = name;
-        speed = 250;
-        fuelEff = 8;
-        fuelTank = 30;
-        seatCnt = 2;
+public class SportCar extends Car implements AirCon, AutoDrive {
+    private boolean isAirCon; // ON/OFF
+    public int setTemp;
+    private boolean isAutoDrv;
+    private boolean isTurbo;
+
+    public SportCar(boolean isAirCon, int setTemp, boolean isAutoDrv,
+                    int speed, String color, String year, boolean isTurbo) {
+        this.isAirCon = isAirCon;
+        this.setTemp = setTemp;
+        this.isAutoDrv = isAutoDrv;
+        this.speed = speed;
+        this.color = color;
+        this.year = year;
+        this.isTurbo = isTurbo;
+    }
+    public void viewInfo(){
+        String airConStr = (isAirCon)? "ON" : "OFF";
+        String autoStr = (isAutoDrv)? "ON" : "OFF";
+        String turboStr = (isTurbo)? "ON" : "OFF";
+        System.out.println("속도 : " + speed);
+        System.out.println("색상 : " + color);
+        System.out.println("연도 : " + year);
+        System.out.println("에어컨 : " + airConStr);
+        System.out.println("온도 : " + autoStr);
+        System.out.println("자율주행 : " + autoStr);
+        System.out.println("터보모드 : " + turboStr);
+
     }
 
     @Override
-    public void setMode(boolean isMode) {
-        if (isMode) speed *= 1.2;
+    public void airConON() {
+        System.out.println("에어컨을 켰습니다.");
+
+    }
+
+    @Override
+    public void airConOFF() {
+        System.out.println("에어컨을 끕니다.");
+    }
+
+    @Override
+    public void setAirConTemp(int tmp) {
+        System.out.println("에어컨 설정 온도를 " + tmp + "로 변경 하였습니다.");
+    }
+
+    @Override
+    public void autoDrvON() {
+        System.out.println("자율 주행 모드를 켰습니다.");
+    }
+
+    @Override
+    public void autoDrvOff() {
+        System.out.println("자율 주행 모드를 껐습니다.");
     }
 }
