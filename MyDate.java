dbe733dc841eacd01ec9eaa04d06cb9fb478583d
SUN <118979741+JSH1990@users.noreply.github.com>
2024-03-25 19:33:04 +0900

    Add files via upload
    //ch2

---
+++ MyDate.java
@@ -0,0 +1,40 @@
+package com.fastcampus.ch2;
+
+public class MyDate {
+    int year;
+    int month;
+    int day;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
+}
