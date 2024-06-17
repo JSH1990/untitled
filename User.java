be6f7554e98adef24098a3d10814459df910e262
SUN <118979741+JSH1990@users.noreply.github.com>
2024-06-17 17:18:04 +0900

    Add files via upload

--- User.java
+++ User.java
@@ -1,70 +1,107 @@
-package com.fastcampus.ch2;
+package com.fastcampus.ch3;
+
+import java.util.Date;
+import java.util.Objects;
 
 public class User {
-   private String id;
-   private String pwd;
-   private String name;
-   private String email;
-   private String birth;
-   private String sns;
+    private String id;
+    private String pwd;
+    private String name;
+    private String email;
+    private Date birth;
+    private String sns;
+    private Date reg_date;
 
-   @Override
-   public String toString() {
-      return "User{" +
-              "id='" + id + '\'' +
-              ", pwd='" + pwd + '\'' +
-              ", name='" + name + '\'' +
-              ", email='" + email + '\'' +
-              ", birth='" + birth + '\'' +
-              ", sns='" + sns + '\'' +
-              '}';
-   }
+    public User(){}
+    public User(String id, String pwd, String name, String email, Date birth, String sns, Date reg_date) {
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.sns = sns;
+        this.reg_date = reg_date;
+    }
 
-   public String getId() {
-      return id;
-   }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && Objects.equals(pwd, user.pwd) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(birth, user.birth) && Objects.equals(sns, user.sns);
+    }
 
-   public void setId(String id) {
-      this.id = id;
-   }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, pwd, name, email, birth, sns, reg_date);
+    }
 
-   public String getPwd() {
-      return pwd;
-   }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birth=" + birth +
+                ", sns='" + sns + '\'' +
+                ", reg_date=" + reg_date +
+                '}';
+    }
 
-   public void setPwd(String pwd) {
-      this.pwd = pwd;
-   }
+    public String getId() {
+        return id;
+    }
 
-   public String getName() {
-      return name;
-   }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+    public String getPwd() {
+        return pwd;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public String getName() {
+        return name;
+    }
 
-   public String getBirth() {
-      return birth;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-   public void setBirth(String birth) {
-      this.birth = birth;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public String getSns() {
-      return sns;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setSns(String sns) {
-      this.sns = sns;
-   }
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getSns() {
+        return sns;
+    }
+
+    public void setSns(String sns) {
+        this.sns = sns;
+    }
+
+    public Date getReg_date() {
+        return reg_date;
+    }
+
+    public void setReg_date(Date reg_date) {
+        this.reg_date = reg_date;
+    }
 }
