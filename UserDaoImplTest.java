c039165f44968d8018420faa85f12d7118134541
SUN <118979741+JSH1990@users.noreply.github.com>
2024-08-28 17:54:00 +0900

    Add files via upload

--- UserDaoImplTest.java
+++ UserDaoImplTest.java
@@ -1,17 +1,18 @@
-package com.fastcampus.ch5.dao;
+package com.fastcampus.ch3;
 
-import com.fastcampus.ch5.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.Assert.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-
-
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class UserDaoImplTest {
     @Autowired
     UserDao userDao;
@@ -21,9 +22,7 @@ public class UserDaoImplTest {
     }
 
     @Test
-    public void selectUser() throws Exception {
-    assertTrue(userDao != null);
-        System.out.println("userDao = " + userDao);
+    public void selectUser() {
     }
 
     @Test
@@ -31,14 +30,26 @@ public class UserDaoImplTest {
     }
 
     @Test
-    public void updateUser() {
-    }
+    public void updateUser() throws Exception {
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(2021, 1, 1);
 
-    @Test
-    public void count() {
-    }
+        userDao.deleteAll();
+        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(cal.getTimeInMillis()), "fb", new Date());
+        int rowCnt = userDao.insertUser(user);
+        assertTrue(rowCnt==1);
 
-    @Test
-    public void deleteAll() {
+        user.setPwd("4321");
+        user.setEmail("bbb@bbb.com");
+        rowCnt = userDao.updateUser(user);
+        assertTrue(rowCnt==1);
+
+        User user2 = userDao.selectUser(user.getId());
+        System.out.println("user = " + user);
+        System.out.println("user2 = " + user2);
+        assertTrue(user.equals(user2));
+
+
     }
 }
