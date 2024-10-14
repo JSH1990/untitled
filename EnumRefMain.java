1ba3d1e8cfc81d8d85f2dcf28f4977689deb6792
SUN <118979741+JSH1990@users.noreply.github.com>
2024-10-14 12:28:57 +0900

    Add files via upload

---
+++ EnumRefMain.java
@@ -0,0 +1,9 @@
+package enumeration.ex3;
+
+public class EnumRefMain {
+    public static void main(String[] args) {
+        System.out.println("Grade.BASIC.getClass() = " + Grade.BASIC.getClass());
+        System.out.println("Grade.GOLD.gCXXXXetClass() = " + Grade.GOLD.getClass());
+        System.out.println("Grade.DIAMOND.getClass() = " + Grade.DIAMOND.getClass());
+    }
+}
---
+++ Grade.java
@@ -0,0 +1,10 @@
+package enumeration.ex3;
+
+/*
+열거형도 클래스이다.
+열거형은 자동으로 java.lang.Enum 을 상속받는다.
+외부에서 임의로 생성할 없다
+ */
+public enum Grade {
+    BASIC, GOLD, DIAMOND
+}
